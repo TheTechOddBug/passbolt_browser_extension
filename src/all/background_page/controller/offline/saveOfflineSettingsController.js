18d952f74407ef24f28adf048d102af7e19b95b7
Sowmya <sowmya@passbolt.com>
2026-05-11 16:41:34 +0200

    PB-51507 OFM - WP5.2 Create SaveOfflineSettingsController

---
+++ src/all/background_page/controller/offline/saveOfflineSettingsController.js
@@ -0,0 +1,56 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.13.0
+ */
+
+import OfflineSettingsEntity from "passbolt-styleguide/src/shared/models/entity/offline/offlineSettingsEntity";
+import SaveOfflineSettingsService from "../../service/offline/saveOfflineSettingsService";
+
+class SaveOfflineSettingsController {
+  /**
+   * @constructor
+   * @param {Worker} worker
+   * @param {string} requestId
+   * @param {ApiClientOptions} apiClientOptions the api client options
+   */
+  constructor(worker, requestId, apiClientOptions) {
+    this.worker = worker;
+    this.requestId = requestId;
+    this.saveOfflineSettingsService = new SaveOfflineSettingsService(apiClientOptions);
+  }
+
+  /**
+   * Controller executor.
+   * @returns {Promise<void>}
+   */
+  async _exec(offlineSettingsDto) {
+    try {
+      const result = await this.exec(offlineSettingsDto);
+      this.worker.port.emit(this.requestId, "SUCCESS", result);
+    } catch (error) {
+      console.error(error);
+      this.worker.port.emit(this.requestId, "ERROR", error);
+    }
+  }
+
+  /**
+   * Save offline settings.
+   * @param {object} offlineSettingsDto The offline settings dto to save
+   * @returns {Promise<OfflineSettingsEntity>} The saved offline settings entity
+   */
+  async exec(offlineSettingsDto) {
+    const offlineSettingsEntity = new OfflineSettingsEntity(offlineSettingsDto);
+    return await this.saveOfflineSettingsService.save(offlineSettingsEntity);
+  }
+}
+
+export default SaveOfflineSettingsController;
---
+++ src/all/background_page/controller/offline/saveOfflineSettingsController.test.js
@@ -0,0 +1,60 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.13.0
+ */
+
+import SaveOfflineSettingsController from "./saveOfflineSettingsController";
+import OfflineSettingsEntity from "passbolt-styleguide/src/shared/models/entity/offline/offlineSettingsEntity";
+import { defaultApiClientOptions } from "passbolt-styleguide/src/shared/lib/apiClient/apiClientOptions.test.data";
+import { defaultOfflineSettingsDto } from "passbolt-styleguide/src/shared/models/entity/offline/offlineSettingsEntity.test.data";
+import EntityValidationError from "passbolt-styleguide/src/shared/models/entity/abstract/entityValidationError";
+
+describe("SaveOfflineSettingsController", () => {
+  let apiClientOptions, controller;
+
+  beforeEach(() => {
+    apiClientOptions = defaultApiClientOptions();
+    controller = new SaveOfflineSettingsController(null, null, apiClientOptions);
+  });
+
+  describe("::exec", () => {
+    it("should save the given offline settings onto the API through the dedicated service", async () => {
+      expect.assertions(3);
+      const offlineSettingsDto = defaultOfflineSettingsDto();
+      const offlineSettingsEntity = new OfflineSettingsEntity(offlineSettingsDto);
+      jest.spyOn(controller.saveOfflineSettingsService, "save").mockResolvedValue(offlineSettingsEntity);
+
+      const result = await controller.exec(offlineSettingsDto);
+
+      expect(result).toEqual(offlineSettingsEntity);
+      expect(controller.saveOfflineSettingsService.save).toHaveBeenCalledTimes(1);
+      expect(controller.saveOfflineSettingsService.save).toHaveBeenCalledWith(offlineSettingsEntity);
+    });
+
+    it("should throw an error if the entity does not validate", async () => {
+      expect.assertions(2);
+      jest.spyOn(controller.saveOfflineSettingsService, "save");
+      const invalidDto = defaultOfflineSettingsDto({ id: 42 });
+
+      await expect(controller.exec(invalidDto)).rejects.toThrow(EntityValidationError);
+      expect(controller.saveOfflineSettingsService.save).not.toHaveBeenCalled();
+    });
+
+    it("should not catch errors and let them be thrown if something wrong happened", async () => {
+      expect.assertions(1);
+      const expectedError = new Error("Something went wrong!");
+      jest.spyOn(controller.saveOfflineSettingsService, "save").mockRejectedValue(expectedError);
+
+      await expect(controller.exec(defaultOfflineSettingsDto())).rejects.toThrow(expectedError.message);
+    });
+  });
+});
